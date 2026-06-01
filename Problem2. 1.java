eec1409603dcd61b724b8bc8e392272a031b1952
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-06-01 23:14:32 +0530

    Create Problem2. 1.java

---
+++ Problem2. 1.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String [] args){
+        int[] numbers = {10,20,30,40};
+    System.out.println(numbers[0]);
+    System.out.println(numbers.length);
+    }
+}
