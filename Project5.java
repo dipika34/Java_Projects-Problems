79b057bc0a72e1aeb9f1df1034deac9613d46b4a
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-06-02 21:03:38 +0530

    Create Problemt5.java

---
+++ Project5.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args){
+        int x = 18, y = 20;
+        if(x > y){
+            System.out.println("x is greater than y");//false condition
+        } else{
+            System.out.println("y is greater than x");
+        }
+    }
+}
