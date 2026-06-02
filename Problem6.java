bf356bf109b1c7f72a811c027fdfce21da717ce0
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-06-02 21:07:34 +0530

    Create Problem6.java

---
+++ Problem6.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args){
+        //if condition is always for true
+        //where else condition is for false.
+        int x = 10,y = 8;
+        if(x==y){
+            System.out.println("x is equal to y");//false condition proves
+        }else{
+            System.out.println("x is not equal to y");//true condition approves.
+        }
+    }
+}
